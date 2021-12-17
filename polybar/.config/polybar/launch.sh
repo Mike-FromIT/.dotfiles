3262853daecd80362bb43025648c59b3ef8daa4b
Michael Coyne <mcoyne0703@pm.me>
2021-12-16 20:38:55 -0600

    Cleared

--- polybar/.config/polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-killall -q polybar
-while pgrep -U $UID -x polybar >/dev/null; do sleep 1; done
-
-
-polybar bar1&
-polybar bar2&
--- vim/.vim_runtime/my_configs.vim
+++
@@ -1,1 +0,0 @@
-autocmd Filetype rmd map <F5> :!echo<space>"require(rmarkdown);<space>render('<c-r>%')"<space>\|<space>R<space>--vanilla<enter>
